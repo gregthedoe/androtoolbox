277ce6c8005415543f63668173e1dbe80ef9f2fa
Greg Doe <gregthedoe@gmail.com>
2016-08-28 13:24:49 +0300

    Allow set loggable level

---
+++ androtoolbox/log.py
@@ -0,0 +1,37 @@
+from adb import adb
+
+_LOG_TAG_PROPERTY = 'log.tag.{tag}'
+
+LOG_LEVELS = ('VERBOSE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'ASSERT')
+
+
+def is_valid_log_level(level):
+    return level.upper() in LOG_LEVELS
+
+
+def set_loggable_level_for_tag(tag, level='VERBOSE'):
+    """
+    Set the minimum loggable level for a tag.
+
+    :param tag: TAG name
+    :param level: Log level.
+    """
+    level = level.upper()
+    if not is_valid_log_level(level):
+        raise ValueError("Unknown log level %s" % level)
+    return adb.set_property(_LOG_TAG_PROPERTY.format(tag=tag), level)
+
+
+def set_loggable_level_for_tags(tags, default_level='VERBOSE'):
+    """
+    Set the minimum log level for a set of tags.
+
+    :param tags: A mapping of tags and their minimum loggable level.
+    :param default_level: If `tags` is a list use this level as the default.
+    """
+    try:
+        for tag, level in tags.iteritems():
+            set_loggable_level_for_tag(tag, level)
+    except AttributeError:
+        for tag in tags:
+            set_loggable_level_for_tag(tag, default_level)
---
+++ scripts/set_loggable_level.py
@@ -0,0 +1,17 @@
+import argparse
+
+from androtoolbox.log import is_valid_log_level, set_loggable_level_for_tags
+
+
+def main():
+    description = "Set the loggable level for a list of tags"
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-l', '--level', default='VERBOSE', help='Log level for all tags')
+    parser.add_argument('tags', metavar='tag', nargs='+', help="Log tag to set")
+    options = parser.parse_args()
+    if not is_valid_log_level(options.level):
+        parser.error("%s is an invalid log level" % options.level)
+    set_loggable_level_for_tags(options.tags, options.level)
+
+if __name__ == "__main__":
+    main()
