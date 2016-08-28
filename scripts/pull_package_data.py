0d763fbf93f2d613f9de9c607e6cb9e60fd3c767
Greg Doe <gregthedoe@gmail.com>
2016-08-28 13:12:22 +0300

    Create scripts

---
+++ scripts/pull_package_data.py
@@ -0,0 +1,29 @@
+import argparse
+import os
+import time
+
+from androtoolbox.packages import pull_data
+
+DEFAULT_DEVICE_TMP_STORAGE = '/data/local/tmp'
+
+
+def main():
+    description = "Pull packages data from a device"
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("package_name", help="Package's name we want to pull")
+    parser.add_argument('-t', "--device-temp-storage", default=DEFAULT_DEVICE_TMP_STORAGE,
+                        help="Device's temp storage used for copying")
+    parser.add_argument('-o', '--output-path', default='.', help="Output path to store packages data in")
+    parser.add_argument('-c', '--create-unique-dir', default=False, action="store_true",
+                        help="Create a unique dir inside the outpath path")
+    options = parser.parse_args()
+    if options.create_unique_dir:
+        output_path = os.path.join(options.output_path, time.strftime("%Y%m%d%H%M"))
+    else:
+        output_path = options.output_path
+    os.makedirs(output_path)
+    pull_data(options.package_name, options.device_temp_storage, output_path)
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import glob
+import os
 from distutils.core import setup
+
+
+scripts = [f for f in glob.glob(os.path.join(__file__, "scripts/*.py")) if  f != '__init__.py']
+
+
 
 setup(
         name='androtoolbox',
@@ -8,5 +15,6 @@ setup(
         license='MIT',
         author='greg',
         author_email='',
-        description='A set of useful tools (or python wrappers) to work with android devices'
+        description='A set of useful tools (or python wrappers) to work with android devices',
+        scripts=scripts,
 )
