7f33c1fad036f1200b99d70b9cfddf0d80c91618
Greg Doe <gregthedoe@gmail.com>
2016-08-28 09:29:07 +0300

    Create basic setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='androtoolbox',
+        version='0.1',
+        packages=['androtoolbox'],
+        url='https://github.com/gregthedoe/androtoolbox',
+        license='MIT',
+        author='greg',
+        author_email='',
+        description='A set of useful tools (or python wrappers) to work with android devices'
+)
