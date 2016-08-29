c477aaa3a2e04f81af3453abe712aa047c99136c
Greg Doe <gregthedoe@gmail.com>
2016-08-29 09:46:23 +0300

    Close the temp shared file

--- androtoolbox/commandline/update_shared_pref.py
+++ androtoolbox/commandline/update_shared_pref.py
@@ -13,16 +13,16 @@ def read_shared_pref(package, pref_name):
 
 
 def write_shared_pref(package, pref_name, shared_pref):
-    tmp_shared_file = tempfile.NamedTemporaryFile()
-    shared_pref.to_file(tmp_shared_file)
-    pref_device_path = build_shared_pref_path(package, pref_name)
-    local_path = tmp_shared_file.name
-    remote_tmp_path = "/data/local/tmp/%s" % os.path.basename(local_path)
-    adb.push(tmp_shared_file.name, remote_tmp_path)
-    adb.shell('mv %s %s' % (remote_tmp_path, pref_device_path), use_su=True)
+    with tempfile.NamedTemporaryFile() as tmp_shared_file:
+        shared_pref.to_file(tmp_shared_file)
+        pref_device_path = build_shared_pref_path(package, pref_name)
+        local_path = tmp_shared_file.name
+        remote_tmp_path = "/data/local/tmp/%s" % os.path.basename(local_path)
+        adb.push(tmp_shared_file.name, remote_tmp_path)
+        adb.shell('mv %s %s' % (remote_tmp_path, pref_device_path), use_su=True)
 
-    # We need to fix any permissions mix up
-    adb.shell('chmod 0777 %s' % pref_device_path, use_su=True)
+        # We need to fix any permissions mix up
+        adb.shell('chmod 0777 %s' % pref_device_path, use_su=True)
 
 
 def update_shared_pref(package, pref_name, updates):
